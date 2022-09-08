962301979b3dcd14488b4c47dd9d4d0060c6d311
frankwangcuo <frankwangcuo@franks-air.dhcp.wustl.edu>
2022-09-08 12:09:18 -0500

    Frank Sept 8

--- src/studio1/HiFour.java
+++ src/studio1/HiFour.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  * From Sedgewick and Wayne, COS 126 course at Princeton
  * 
  */
-public class HiFour {
+public class HiFour{
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		System.out.print("Enter value for arg 0: ");
@@ -20,6 +20,7 @@ public class HiFour {
 		//
 		// Say hello to the names in s0 through s3.
 		//
+		System.out.println("Greetings " + s0 + ", " + s1 + ", " + s2 + ", and " + s3 + ". How are you?");
 
 	}
 }
